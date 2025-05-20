ed47749a2d47e2747c14af9fe21afe8331c3cf88
ajay0180 <jangraajay489@gmail.com>
2025-05-20 17:48:51 +0530

    initial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { ItemsList } from "./components/ItemsList";
 
 function App() {
-  return <div className="text-blue-500">Hello world</div>;
+  return (
+    <div className="text-blue-500">
+      <ItemsList />
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/ItemsList.jsx
+++ frontend/src/components/ItemsList.jsx
@@ -1,13 +1,34 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { API_ENDPOINT } from "../utils/constants";
 
 export const ItemsList = () => {
   const [lostItems, setLostItems] = useState([]);
-  useEffect(() => {
-    const response = fetch(`${API_ENDPOINT}/getAllItems`);
-    console.log(response);
 
-    setLostItems(lostItems);
-  }, [setLostItems]);
-  return <div></div>;
+  const getItems = async () => {
+    try {
+      const response = await axios.get(`${API_ENDPOINT}/getAllItems`);
+      setLostItems(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Failed to fetch items:", error);
+    }
+  };
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
+  return (
+    <div>
+      {lostItems.map((el) => (
+        <div key={el._id}>
+          {" "}
+          {/* add unique key */}
+          <div>{el.ItemName}</div>
+          <div>{el.description}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
